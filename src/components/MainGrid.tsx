a0c5dd3692f60382e5fd43c7783e4c70e80df5ed
VHrushi <ihrushey@VHrushis-MacBook-Pro.local>
2024-09-27 15:21:21 +0530

    usage in maingrid

--- src/components/MainGrid.tsx
+++ src/components/MainGrid.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Copyright from '../internals/components/Copyright';
 import { IconSplashGenerator } from 'icon-splash-generator';
 import { useGlobalState } from 'icon-splash-generator';
-import React from 'react';
 
-export default function MainGrid() {
-  const { state: globalState } = useGlobalState();
+const MainGrid = () => {
+  const { state } = useGlobalState();
+  const { selectedBackground, selectedIconSize } = state;
 
   React.useEffect(() => {
-    console.log('Global state updated from library:', globalState);
-  }, [globalState]);
+    console.log('Selected background updated:', selectedBackground);
+    console.log('Selected icon size:', selectedIconSize);
+  }, [selectedBackground, selectedIconSize]);
 
   return (
     <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
@@ -21,4 +23,6 @@ export default function MainGrid() {
       <Copyright sx={{ my: 4 }} />
     </Box>
   );
-}
+};
+
+export default MainGrid;
